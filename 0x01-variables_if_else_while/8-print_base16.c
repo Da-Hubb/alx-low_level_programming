8be38c854f825d777f83dc0ec9576fd804de1757
Josemaria <josemariaofurum@gmail.com>
2023-02-17 13:46:54 -0800

    hexadecimal (task 08);

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int x;
+
+	char a;
+
+	x = 0;
+	a = 'a';
+	while (x < 10)
+	{
+		putchar(x);
+		x++;
+	}
+	while (a <= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
