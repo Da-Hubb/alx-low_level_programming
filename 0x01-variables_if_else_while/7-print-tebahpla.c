1fc2b6d2e229ace0225638f35a6f914fe0ad0718
Josemaria <josemariaofurum@gmail.com>
2023-02-17 13:36:41 -0800

    print alphabets backwards (task 07)

---
+++ 0x01-variables_if_else_while/7-print-tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: success
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a <= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
